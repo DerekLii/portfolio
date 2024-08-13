2c1fa238c8e52001ce6653da9276377e50aea13d
Derek Li <derekli33@hotmail.com>
2024-08-13 00:02:05 -0400

    new

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Project from "./Project";
 import linkedin from "../images/linkedin.jpg";
-import eightysix from "../images/86.jpg";
+import discord from "../images/discord.jpg";
 
 const Projects = () => {
   return (
     <div id="projects" className="bg-white md:w-1/2 mx-auto mb-10 px-4">
       <div className="text-2xl font-medium text-black text-left mb-5">Projects</div>
       <div className="flex flex-wrap justify-center items-center gap-2">
-        <Project image={eightysix} link="https://dereklii.github.io/Eighty-Six/index.html" />
+        <Project image={discord} link="https://github.com/DerekLii/DiscordBot" />
         <Project image={linkedin} />
         <Project image={linkedin} />
         <Project image={linkedin} />
